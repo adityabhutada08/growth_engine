282da01a2b9711b033dd5295915c087984bcabcf
adityabhutada08 <adityabhutada08@gmail.com>
2026-03-03 19:56:38 +0530

    UI changes

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import dashboard_home, leads_list
+from .views import dashboard_home, leads_list, dashboard_leads, update_status
 
 urlpatterns = [
     path('', dashboard_home, name='dashboard-home'),
     path('leads/', leads_list, name='dashboard-leads'),
+    path('dashboard/leads/', dashboard_leads, name='dashboard-leads'),
+    path('update-status/<uuid:lead_id>/<str:new_status>/', update_status, name='update-status'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from leads.models import Lead
-from django.db.models import Count
+from django.db.models import Count, Sum
+from django.db.models import Q
+from django.shortcuts import redirect
+
+
+def update_status(request, lead_id, new_status):
+
+    lead = Lead.objects.get(id=lead_id)
+    lead.status = new_status
+    lead.save()
+
+    return redirect('dashboard-leads')
 
 
 def leads_list(request):
@@ -22,11 +33,42 @@ def dashboard_home(request):
         total=Count('id')
     )
 
+    sales_leaderboard = Lead.objects.filter(
+        status='converted').values(
+        'assigned_to__name').annotate(
+        total_revenue=Sum('revenue')).order_by('-total_revenue')
+
     context = {
         "total_leads": total_leads,
         "converted": converted,
         "conversion_rate": conversion_rate,
         "leads_by_campaign": leads_by_campaign,
+        "sales_leaderboard": sales_leaderboard,
     }
 
     return render(request, "dashboard/dashboard.html", context)
+
+
+def dashboard_leads(request):
+
+    query = request.GET.get('search')
+    status_filter = request.GET.get('status')
+
+    leads = Lead.objects.all().order_by('-created_at')
+
+    if query:
+        leads = leads.filter(
+            Q(name__icontains=query) |
+            Q(phone__icontains=query) |
+            Q(email__icontains=query)
+        )
+
+    if status_filter:
+        leads = leads.filter(status=status_filter)
+
+    context = {
+        "leads": leads,
+        "status_choices": Lead.STATUS_CHOICES
+    }
+
+    return render(request, "dashboard/leads.html", context)
